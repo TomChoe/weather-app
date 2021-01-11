81d9c8ba93c08d8bc069237d81c6a0fdfdc3b061
tomchoe <doodeitstom@yahoo.com>
2021-01-10 21:33:54 -0600

    Implemented request http lib which is deprec.  Axios to replace once calls are setup

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-console.log('Starting');
+const { clear } = require('console');
+const request = require('request');
 
+const url = 'http://api.weatherstack.com/current?access_key=1a0f2534f7732b46457fdcb66a6b1881&query=Dallas';
 
-
-
-
-console.log('Stopping');
+request({url: url}, (err, res) => {
+    const data = JSON.parse(res.body);
+    console.log(data.current);
+})
